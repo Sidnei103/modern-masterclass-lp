794d523fe48064007648c2079484c7113bbee708
sidnei <sidnei.junior@academiademetas.com.br>
2025-02-05 20:02:58 -0300

    Adiciona .npmrc e atualiza next.config.mjs para resolver conflitos de dependências"

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,13 @@ const nextConfig = {
       },
     ],
   },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 export default nextConfig
-
