e59376e19a6b572e02b75ec859a38989fa7e5f75
sidnei <sidnei.junior@academiademetas.com.br>
2025-02-05 20:06:09 -0300

    Simplifica next.config.mjs para resolver erro de build

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,7 @@ const nextConfig = {
       },
     ],
   },
-  compress: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 export default nextConfig
